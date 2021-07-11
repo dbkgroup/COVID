9c408210df8ca2d6422d38106f2d629a9e5dc2de
Ivayla Roberts <ivayla-roberts@outlook.com>
2021-07-11 12:19:15 +0100

    PCA plots

---
+++ Data_viz.R
@@ -0,0 +1,124 @@
+#To install ggbiplot, uncomment line 3 and 4 and run only once, comment them back once done
+
+#library(devtools)
+#install_github("vqv/ggbiplot")
+library(ggbiplot)
+
+library(ggpubr)
+library(gridExtra)
+library(ggrepel)
+library(RColorBrewer)
+
+
+# Basic PCA plot function based on ggbiplot
+i_plotPCA <- function (pca, title, groups = NULL, colorsName = NULL, colors = NULL, ellipse = F, circle = F, labels = NULL, alpha = 1, title_center = T) {
+  
+  plot <- ggbiplot(pca, obs.scale = 1, var.scale = 1, alpha = alpha,
+                   groups = as.factor(groups), ellipse = ellipse, circle = circle, var.axes = F, labels = labels) + 
+    ggtitle(title) +
+    theme_pubr(base=9, base_family = "sans", border = F) +
+    theme(legend.direction = 'horizontal', legend.position = 'none')
+  
+  if(! is.null(colors))
+    plot <- plot + scale_color_manual(name = colorsName, breaks = colors$breaks, values = colors$values) 
+  
+  if(title_center) 
+    plot <- plot + theme(plot.title = element_text(hjust = 0.5), axis.text = element_text(color = "gray20", size = 8))
+  
+  return(plot)
+}
+
+
+# Overload of i_plotPCA with focus on LC-MS batches
+get_pca_by_batch <- function(eset, group_filter, labels = F, title = "By batch", title_center = T) {
+  
+  eset2 <- eset[,eset@phenoData$Group %in% group_filter]
+  eset2 <- eset2[,!(eset2@phenoData$Sample_name %in% c("IQA50", "IQA75", "IQA125", "IQA150", "SQA50", "SQA75", "SQA125", "SQA150"))]
+  
+  pca_pos_a <- prcomp(t(exprs(eset2)), scale. = TRUE)
+  
+  #PCAcolors <- data.frame(breaks = c("B1", "B2", "B3", "B4", "B5"), 
+  #                         values = c("midnightblue", "lightcoral", "gray50", "mediumaquamarine", "dodgerblue"))
+  
+  pD <- pData(eset2)
+  col <- pull(pD, Batch)
+  PCAcolors <- data.frame(breaks = unique(col), 
+                          values = brewer.pal(length(unique(col)), "Dark2")[1:length(unique(col))])
+  
+  if(labels) 
+    p1 <- i_plotPCA(pca_pos_a, title, groups = eset2@phenoData$Batch, colorsName = "Sample type", colors = PCAcolors, alpha = 0.7, labels = eset2@phenoData$Sample_name, title_center = title_center)
+  
+  else 
+    p1 <- i_plotPCA(pca_pos_a, title, groups = eset2@phenoData$Batch, colorsName = "Sample type", colors = PCAcolors, alpha = 0.7, title_center = title_center)
+  
+  p1 <- p1 + theme(legend.direction = 'horizontal', legend.position = 'top', legend.title = element_blank())
+  
+  p1
+}
+
+# Overload of i_plotPCA with focus on sample groups (Sample, QC, IQA, SQA, Blanks)
+get_pca_by_group <- function(eset, group_filter, labels = F, title = "By group", title_center = T) {
+  
+  
+  
+  eset2 <- eset[,eset@phenoData$Group %in% group_filter]
+  eset2 <- eset2[,!(eset2@phenoData$Sample_name %in% c("IQA50", "IQA75", "IQA125", "IQA150", "SQA50", "SQA75", "SQA125", "SQA150"))]
+  
+  pca_pos_a <- prcomp(t(exprs(eset2)), scale. = TRUE)
+  
+  #PCAcolors <- data.frame(breaks = c("QC", "Sample", "Blank", "IQA", "SQA"), 
+  #                         values = c("mediumaquamarine", "lightcoral", "gray50", "midnightblue", "dodgerblue"))
+  
+  pD <- pData(eset2)
+  col <- pull(pD, Group)
+  uni <- unique(col)
+  
+  #return()
+  PCAcolors <- data.frame(breaks = uni, 
+                          values = brewer.pal(length(uni), "Dark2")[1:length(uni)])
+  
+  if(labels) 
+    p1 <- i_plotPCA(pca_pos_a, title, groups = eset2@phenoData$Group, colorsName = "Sample type", colors = PCAcolors, alpha = 0.7, labels = eset2@phenoData$Sample_name, title_center = title_center)
+  else
+    p1 <- i_plotPCA(pca_pos_a, title, groups = eset2@phenoData$Group, colorsName = "Sample type", colors = PCAcolors, alpha = 0.7, title_center = title_center)
+  
+  p1 <- p1 + theme(legend.direction = 'horizontal', legend.position = 'top', legend.title = element_blank())
+  
+  p1
+}
+
+
+# Overload of i_plotPCA with focus on whatever desired column in the Expression_set@phenoData
+get_pca_by_custom <- function(eset, group_filter, by, labels = F, title = as.character(NA), title_center = T, breaks_list = c()) {
+  
+  if(is.na(title))
+    title <- paste("By", by, sep = " ")
+  
+  eset2 <- eset[,eset@phenoData$Group %in% group_filter]
+  eset2 <- eset2[,!(eset2@phenoData$Sample_name %in% c("IQA50", "IQA75", "IQA125", "IQA150", "SQA50", "SQA75", "SQA125", "SQA150"))]
+  
+  pca_pos_a <- prcomp(t(exprs(eset2)), scale. = TRUE)
+  
+  pD <- pData(eset2)
+  col <- pull(pD, {{by}})
+  brs <- unique(col)
+  if(length(breaks_list) > 0)
+    brs <- breaks_list
+  
+  PCAcolors <- data.frame(breaks = brs, 
+                          values = brewer.pal(length(unique(col)), "Dark2")[1:length(brs)])
+  
+  
+  if(labels) 
+    p1 <- i_plotPCA(pca_pos_a, title = title, groups = col, colorsName = "Sample type", colors = PCAcolors, alpha = 0.8, labels = eset2@phenoData$Sample_name, title_center = title_center)
+  else
+    p1 <- i_plotPCA(pca_pos_a, title = title, groups = col, colorsName = "Sample type", colors = PCAcolors, alpha = 0.8, title_center = title_center)
+  
+  p1 <- p1 + theme(legend.direction = 'horizontal', legend.position = 'top', legend.title = element_blank())
+  
+  p1
+}
+
+
+
+
